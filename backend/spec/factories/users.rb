a0559a430d5079734fcca634c33026e4d504d95e
Aki <sekai.110.0604@gmail.com>
2025-11-28 23:36:07 +0000

    fix: FactoryBotのUserファクトリーにname属性を追加
    - Userモデルにnameのバリデーションが追加されたため、FactoryBotにもnameを追加
    - これによりテストでユーザーを作成する際にバリデーションエラーが発生しなくなる

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
+    name { 'Test User' }
     password { 'password123' }
     password_confirmation { 'password123' }
 
